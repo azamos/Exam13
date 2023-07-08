55015254adb06854ca00a5397cac43265b15bec4
amos zohar <azamoszohar@gmail.com>
2023-07-08 17:04:34 +0300

    finished q1

--- Q1.c
+++ Q1.c
@@ -2,5 +2,15 @@
 
 int getNumFlowers(int mat[][COLS], int rows)
 {
-	return -1;
+	int sum = 0;
+	for (int i = 1; i < rows - 1; i++) {
+		for (int j = 1; j < COLS - 1; j++) {
+			int sumPetals = mat[i - 1][j - 1]
+				+ mat[i - 1][j + 1] + mat[i + 1][j - 1] + mat[i + 1][j + 1];
+			if (mat[i][j] == sumPetals) {
+				sum++;
+			}
+		}
+	}
+	return sum;
 }
--- Q3.c
+++ Q3.c
@@ -6,12 +6,12 @@ float distance( Location l1, Location l2 )
 	return (float)sqrt( pow( l1.x - l2.x, 2 ) + pow( l1.y - l2.y, 2 ) );
 }
 
-texi* findTexi(Yango* yango, Client* client, int maxDistance, int *maxPrice)
+Texi* findTexi(Yango* yango, Client* client, int maxDistance, int *maxPrice)
 {
 	return NULL;
 }
 
-Clients **getClientsFromSpecificDates( Yango* yango, char *firstTime, char *secondTime, int *numClients)
+Client** getClientsFromSpecificDates( Yango* yango, char *firstTime, char *secondTime, int *numClients)
 {
 	return NULL;
 }
